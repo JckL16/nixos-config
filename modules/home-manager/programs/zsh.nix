9c72a91e0ac89aa4ab6601694360929a00ea44b3
Jack <jack@example.com>
2025-10-15 21:34:32 +0200

    Added a new plugin for zsh for autocorrection after writing a command

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -1,24 +1,34 @@
 # modules/home-manager/zsh.nix
-
 { pkgs, lib, config, variables, ... }: {
-  
   home.packages = with pkgs; [
     fzf
+    pay-respects
   ];
-
+  
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    
     oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-        plugins = [
-            "git"
-            "fzf"
-        ];
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "fzf"
+      ];
     };
+    
+    shellAliases = {
+      switch = "sudo nixos-rebuild switch --flake ~/nixos-config";
+      test = "sudo nixos-rebuild test --flake ~/nixos-config";
+      update = "nix flake update ~/nixos-config && sudo nixos-rebuild switch --flake ~/nixos-config";
+      fuck = "f";
+    };
+    
+    initContent = ''
+      eval "$(pay-respects --alias --shell zsh)"
+    '';
   };
-
 }
