e4b3ceb133e5abee189a12994552caa22a66125f
Jack <jack@example.com>
2025-10-25 13:54:27 +0200

    More cleanup of the config

--- modules/nixos/desktop/sway.nix
+++ modules/nixos/desktop/sway.nix
@@ -4,62 +4,40 @@
   options = {
     sway.enable = lib.mkEnableOption "Enable sway window manager";
   };
-
   config = lib.mkIf config.sway.enable {
     # Printing support for Sway
     services.printing.enable = true;
     
     security.polkit.enable = true;
-
     security.pam.services.swaylock = {};
-
+    
     programs.sway = {
       enable = true;
       package = pkgs.swayfx;
+      # These packages need system-level installation for proper integration
       extraPackages = with pkgs; [
-        swaylock-effects
+        swaylock-effects  # Needs PAM integration
         swayidle
         wl-clipboard
-        rofi
-        mako
-        waybar
-        swaybg
-        grim
-        slurp
-        brightnessctl
-        networkmanagerapplet
       ];
     };
-
+    
     xdg.portal = {
       enable = true;
       wlr.enable = true;
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     };
-
+    
     services.displayManager.ly = {
       enable = true;
     };
-
+    
+    # Only system-critical packages
     environment.systemPackages = with pkgs; [
-      swaylock-effects
-      swayidle
-      wl-clipboard
-      rofi
-      mako
-      waybar
-      swaybg
-      swayfx
-      grim
-      slurp
-      brightnessctl
-      networkmanagerapplet
-      nerd-fonts.jetbrains-mono
       wayland
-      wl-clipboard
-      qalculate-gtk
+      qalculate-gtk  # System calculator
     ];
-
+    
     services.udisks2.enable = true;
   };
 }
