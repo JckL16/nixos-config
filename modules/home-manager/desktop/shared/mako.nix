349fef1b6a5c22467dab1fb02b54114c4ecffb89
Jack <jack@example.com>
2025-10-28 20:07:42 +0100

    Made some changes to the notification deamon

--- modules/home-manager/desktop/shared/mako.nix
+++ modules/home-manager/desktop/shared/mako.nix
@@ -1,7 +1,7 @@
 # modules/home-manager/shared/mako.nix
 
 { pkgs, lib, config, ... }: {
-
+  
   config = lib.mkIf (config.sway.enable || config.hyprland.enable) {
     services.mako = {
       enable = true;
@@ -10,13 +10,26 @@
         background-color = "#2E3440";
         border-color = "#4C566A";
         text-color = "#D8DEE9";
-        border-size = 1;
-        border-radius = 0;
+        border-size = 1;        border-radius = 3;
         padding = "8";
         margin = "8";
         max-visible = 5;
         sort = "-time";
+        group-by = "app-name";
       };
+      extraConfig = ''
+        [app-name="Spotify"]
+        default-timeout=10000
+
+        [app-name="blueman"]
+        default-timeout=5000
+
+        [app-name="Discord"]
+        default-timeout=10000
+
+        [app-name="Steam"]
+        default-timeout=10000
+      '';
     };
   };
 }
