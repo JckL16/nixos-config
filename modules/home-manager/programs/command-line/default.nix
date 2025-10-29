9fb337a0aeb8836e9be20112d9f750e24cae3451
Jack <jack@example.com>
2025-10-29 16:09:20 +0100

    Refactored and moved the modules in home-manager/programs into more specific directories

---
+++ modules/home-manager/programs/command-line/default.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  
+  imports = [
+    ./git.nix
+    ./zsh.nix
+    ./neofetch.nix
+    ./nvim.nix
+  ];
+
+}
--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -1,15 +1,10 @@
 { ... }: {
   
   imports = [
-    ./alacritty.nix
-    ./git.nix
-    ./zsh.nix
-    ./neofetch.nix
-    ./steam.nix
-    ./libreoffice.nix
-    ./onlyoffice.nix
-    ./nvim.nix
-    ./gamemode.nix
+    ./command-line
+    ./desktop
+    ./gaming
+    ./terminals
   ];
 
 }
