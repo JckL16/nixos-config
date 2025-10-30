29caf4143b56f256c2d0444d98bf938112bc78df
Jack <jack@example.com>
2025-10-30 12:47:19 +0100

    Added some more functionality to the zsh shell

--- modules/home-manager/programs/command-line/zsh.nix
+++ modules/home-manager/programs/command-line/zsh.nix
@@ -1,10 +1,19 @@
-# modules/home-manager/programs/zsh.nix
+# modules/home-manager/programs/command-line/zsh.nix
 
 { pkgs, lib, config, variables, ... }: {
   home.packages = with pkgs; [
     fzf
     pay-respects
     eza
+    bat
+    ripgrep
+    fd
+    tldr
+    btop
+    duf
+    dust
+    procs
+    delta
   ];
   
   programs.zsh = {
@@ -19,28 +28,76 @@
       plugins = [
         "git"
         "fzf"
+        "sudo"
+        "command-not-found"
+        "colored-man-pages"
+        "extract"
       ];
     };
     
     shellAliases = {
+      # Nixos aliases
       switch = "sudo nixos-rebuild switch --flake ~/nixos-config";
       test = "sudo nixos-rebuild test --flake ~/nixos-config";
       dry-run = "sudo nixos-rebuild dry-run --flake ~/nixos-config";
       update = "nix flake update --flake ~/nixos-config && sudo nixos-rebuild switch --flake ~/nixos-config";
       clean = "nix-collect-garbage";
       install-bootloader = "sudo nixos-rebuild boot --install-bootloader --flake ~/nixos-config";
+      
+      # Pay pay-respects alias (correction of earlier written command)
       fuck = "f";
-
-      # Eza aliases
+      
+      # Eza aliases (ls replacement)
       ls = "eza --icons --group-directories-first";
       ll = "eza --icons --group-directories-first -l";
       la = "eza --icons --group-directories-first -la";
       lt = "eza --icons --group-directories-first --tree";
       tree = "eza --icons --group-directories-first --tree";
+      
+      # Bat alias (cat replacement)
+      cat = "bat";
+      
+      # Zoxide alias (cd replacement)
+      cd = "z";
+      
+      # Git shortcuts
+      gst = "git status";
+      gco = "git checkout";
+      gp = "git push";
+      gl = "git pull";
+      
+      # Navigation
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      
+      # Safety nets
+      rm = "rm -i";
+      cp = "cp -i";
+      mv = "mv -i";
+      
+      # Nix helpers
+      nix-search = "nix search nixpkgs";
     };
     
     initContent = ''
       eval "$(pay-respects --alias --shell zsh)"
+      
+      # Better history search with fzf
+      bindkey '^R' fzf-history-widget
+      
+      # Better directory navigation
+      setopt AUTO_CD              # Type directory name to cd
+      setopt AUTO_PUSHD           # Make cd push old directory onto stack
+      setopt PUSHD_IGNORE_DUPS    # Don't push duplicates
+      
+      # Better globbing
+      setopt EXTENDED_GLOB
     '';
+  };
+  
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
   };
 }
