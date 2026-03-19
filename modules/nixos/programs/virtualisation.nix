8909794d8034cdec2d4b04fa8f66a2f65dcaa0ff
Jack <jack@example.com>
2026-03-19 10:49:29 +0100

    Added a service that makes sure the NAT interface of QEMU gets started on boot

--- modules/nixos/programs/virtualisation.nix
+++ modules/nixos/programs/virtualisation.nix
@@ -23,5 +23,21 @@
     virtualisation.libvirtd.onBoot = "start";
     virtualisation.libvirtd.onShutdown = "shutdown";
 
+    # Ensure the default NAT network starts automatically
+    systemd.services.libvirtd-default-network = {
+      description = "Autostart libvirt default NAT network";
+      after = [ "libvirtd.service" ];
+      requires = [ "libvirtd.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = ''
+        ${pkgs.libvirt}/bin/virsh net-autostart default || true
+        ${pkgs.libvirt}/bin/virsh net-start default || true
+      '';
+    };
+
   };
 }
