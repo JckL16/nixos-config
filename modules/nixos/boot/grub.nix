84dd20b755e2f849adb55983fdbcd1cd3f23aea0
Jack <jack@example.com>
2026-04-22 00:04:29 +0200

    Fix grub.nix: add mirroredBoots for UEFI when disko disabled
    UEFI systems with device="nodev" require mirroredBoots to be set.
    This fixes the assertion error for hosts without diskoConfig enabled.

--- modules/nixos/boot/grub.nix
+++ modules/nixos/boot/grub.nix
@@ -7,21 +7,36 @@
       lib.mkEnableOption "Enable grub";
   };
 
-  config = lib.mkIf config.grub.enable {
-    boot.loader = {
+  config = lib.mkIf config.grub.enable (lib.mkMerge [
+    # Base GRUB config (always applied)
+    {
+      boot.loader = {
         grub = {
-            enable = true;
-            useOSProber = true;
-            configurationLimit = 5;
-        } // lib.optionalAttrs (!config.diskoConfig.enable) {
-            # Only set these if disko is NOT enabled (disko handles bootloader config)
-            device = variables.bootDevice;
-            efiSupport = !variables.isBIOS;
+          enable = true;
+          useOSProber = true;
+          configurationLimit = 5;
         };
         timeout = 5;
-    } // lib.optionalAttrs (!config.diskoConfig.enable) {
-        efi.canTouchEfiVariables = !variables.isBIOS;
-    };
-  };
+      };
+    }
+
+    # Fallback config when disko is NOT enabled
+    (lib.mkIf (!config.diskoConfig.enable) (
+      if variables.isBIOS then {
+        # BIOS: install GRUB to the disk
+        boot.loader.grub.device = variables.bootDevice;
+        boot.loader.grub.efiSupport = false;
+      } else {
+        # UEFI: use mirroredBoots for EFI installation
+        boot.loader.grub.device = "nodev";
+        boot.loader.grub.efiSupport = true;
+        boot.loader.grub.mirroredBoots = [{
+          devices = [ "nodev" ];
+          path = "/boot";
+        }];
+        boot.loader.efi.canTouchEfiVariables = true;
+      }
+    ))
+  ]);
 
 }
