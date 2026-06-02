aa0164b549242afcfb23aa2738cffca19bdb1352
Jack <jack@example.com>
2026-06-02 13:41:23 +0200

    feat(nvim): set 4-space indent for C and C++ files

--- modules/home-manager/programs/command-line/nvim/config/options.lua
+++ modules/home-manager/programs/command-line/nvim/config/options.lua
@@ -21,6 +21,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 4
   end,
 })
+
+-- C: use 4-space indent
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
 vim.opt.wrap = false
 
 -- Markdown: enable line wrap for readability
